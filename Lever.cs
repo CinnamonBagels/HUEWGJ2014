08eb25a91868d41574f9359e1785091276725947
CinnamonBagels <samuelsko@gmail.com>
2014-01-25 21:57:11 -0800

    init commit

---
+++ Lever.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Duality;
+using Duality.Components.Physics;
+using Duality.Resources;
+using OpenTK;
+using OpenTK.Input;
+
+namespace Duality_ {
+	[Serializable]
+	public class Lever : Component, ICmpUpdatable, ICmpInitializable  {
+		
+		public float time = 1.0f;
+		public float lastTime = Time.GameTimer.Seconds;
+		public float currentTime;
+		public void OnUpdate() {
+
+			RigidBody body = this.GameObj.RigidBody;
+
+			if (DualityApp.Keyboard[Key.L] && currentTime - lastTime >= 1.0f) {
+				//get a list of all gameobjects with script Block
+				//GameObject[] listOfBlock = Scene.Current.FindGameObjects<Block>().ToArray<GameObject>();
+
+				// 1. Retrieve 
+				ContentRef<Prefab> blockPrefabReference = GameRes.Data.Mario_Block_Prefab;
+				Prefab blockPrefab = blockPrefabReference.Res;
+
+				// 2. Instantiate 
+				GameObject blockObject = blockPrefab.Instantiate();
+				//blockObject.Parent = this.GameObj; 
+
+				// 3. Configure 
+				blockObject.Transform.Pos = new Vector3( 0, 0, -50 ); 
+				//blockObject.Transform.Angle = this.GameObj.Transform.Angle; 
+
+				// 4. Add 
+				Scene.Current.AddObject(blockObject);
+
+				lastTime = Time.GameTimer.Seconds;
+			}
+
+			currentTime = Time.GameTimer.Seconds;
+		}
+
+		public void OnInit(Component.InitContext context) {
+		}
+
+		public void OnShutdown(Component.ShutdownContext context) {
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Mario.cs
@@ -0,0 +1,40 @@
+﻿using Duality;
+using Duality.Components.Physics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using OpenTK;
+using OpenTK.Input;
+
+namespace Duality_ {
+	public class Mario : Component, ICmpUpdatable, ICmpInitializable {
+
+		private bool isCollidingWithLever;
+
+		public void OnUpdate() {
+
+			RigidBody body = this.GameObj.RigidBody;
+			RigidBody lever;
+			RigidBody block;
+
+			if (DualityApp.Keyboard[Key.A]) {
+				body.ApplyLocalForce(-0.0001f);
+			} else if (DualityApp.Keyboard[Key.D]) {
+				body.ApplyLocalForce(0.0001f);
+			}
+
+			if (DualityApp.Keyboard[Key.L] && isCollidingWithLever) {
+
+			}
+		}
+
+		public void OnInit(Component.InitContext context) {
+			isCollidingWithLever = false;
+		}
+
+		public void OnShutdown(Component.ShutdownContext context) {
+		}
+	}
+}
