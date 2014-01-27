eba668cce7aab4e3fc21b95ca146ab525cec887c
CinnamonBagels <samuelsko@gmail.com>
2014-01-26 17:18:57 -0800

    GAME IS FINISHED .zip folder

---
+++ PoisonBlock.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Duality;
+
+namespace Space_Station_Escape {
+	[Serializable]
+	class PoisonBlock : Component, ICmpCollisionListener {
+
+		private float damageAmount = 5.0f;
+		
+		public void OnCollisionBegin(Component sender, CollisionEventArgs args) {
+			Component player = sender.GameObj.GetComponent<Player>();
+			player.takeDamage(damageAmount);
+		}
+
+		public void OnCollisionEnd(Component sender, CollisionEventArgs args) {
+			throw new NotImplementedException();
+		}
+
+		public void OnCollisionSolve(Component sender, CollisionEventArgs args) {
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ WinBlock.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Duality;
+
+namespace Space_Station_Escape {
+	class WinBlock : Component, ICmpCollisionListener {
+
+		public void OnCollisionBegin(Component sender, CollisionEventArgs args) {
+			//you win!
+		}
+
+		public void OnCollisionEnd(Component sender, CollisionEventArgs args) {
+			throw new NotImplementedException();
+		}
+
+		public void OnCollisionSolve(Component sender, CollisionEventArgs args) {
+			throw new NotImplementedException();
+		}
+	}
+}
